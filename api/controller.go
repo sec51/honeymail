d94cd9f99b88fb91d917882f28a85650c9068c87
silenteh <silenteh@gmail.com>
2016-04-17 20:07:54 +0200

    Added simple authorization mechanism via config file and ip address

--- api/controller.go
+++ api/controller.go
@@ -3,13 +3,16 @@ package api
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/sec51/goconf"
 	"io"
 	"log"
+	"net"
 	"net/http"
 )
 
 var null = []byte("null")
 var empty = []byte("{}")
+var validIPs = goconf.AppConf.DefaultStrings("http.allowed_hosts", []string{"127.0.0.1"})
 
 type output interface {
 	JSON(data interface{}, w http.ResponseWriter, r *http.Request)
@@ -20,7 +23,36 @@ type SimpleController struct {
 	output
 }
 
+func arrayContains(ip string, validationSet []string) bool {
+
+	for _, entry := range validationSet {
+		if ip == entry {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isValidIP(ip string) bool {
+
+	host, _, err := net.SplitHostPort(ip)
+	if err != nil {
+		return false
+	}
+
+	return arrayContains(host, validIPs)
+
+}
+
 func (c SimpleController) JSON(data interface{}, w http.ResponseWriter, r *http.Request) {
+
+	// if the ip is invalid then return 404
+	if !isValidIP(r.RemoteAddr) {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	var content []byte
 	var err error
@@ -42,6 +74,12 @@ func (c SimpleController) JSON(data interface{}, w http.ResponseWriter, r *http.
 }
 
 func (c SimpleController) Error500(err error, w http.ResponseWriter, r *http.Request) {
+
+	if !isValidIP(r.RemoteAddr) {
+		http.NotFound(w, r)
+		return
+	}
+
 	if err == io.EOF {
 		http.Error(w, "Item not found", http.StatusInternalServerError)
 		return
